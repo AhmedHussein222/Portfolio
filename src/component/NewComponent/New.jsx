3cfc358f11544689ab8d7b13f53b97329be36972
Ahmed Hussein <Ahmdhussein222@gmail.com>
2025-03-17 12:49:58 +0200

    add alert

--- src/component/NewComponent/New.jsx
+++ src/component/NewComponent/New.jsx
@@ -7,7 +7,16 @@ function New() {
         <div className="container">
             <h1>New Component</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil aliquid dolor sapiente, inventore magni et? Rem nemo quo est nostrum, dolorum vel mollitia molestiae sunt ipsum soluta harum velit laborum?</p>
-
+            
+            <div
+                class="alert alert-primary"
+                role="alert"
+            >
+                <strong>Warning!</strong
+                ><a href="#" class="alert-link">Click Here</a>
+            </div>
+            
+            
         </div>
     </Div>
     
