e83f51ffc2beafd69786c326d649bab095dd8a7e
Ahmed Hussein <Ahmdhussein222@gmail.com>
2025-03-17 12:42:36 +0200

    add paragraph after header

--- src/component/NewComponent/New.jsx
+++ src/component/NewComponent/New.jsx
@@ -6,6 +6,8 @@ function New() {
     <Div>
         <div className="container">
             <h1>New Component</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil aliquid dolor sapiente, inventore magni et? Rem nemo quo est nostrum, dolorum vel mollitia molestiae sunt ipsum soluta harum velit laborum?</p>
+
         </div>
     </Div>
     
